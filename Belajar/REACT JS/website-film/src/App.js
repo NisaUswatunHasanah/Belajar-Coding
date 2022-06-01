2a55deb40bcb070b2ed8e6ad3374161546fa8ef3
NisaUswatunHasanah <nisaneyzo@gmail.com>
2022-06-01 10:39:59 +0700

    update

--- Belajar/REACT JS/website-film/src/App.js
+++ Belajar/REACT JS/website-film/src/App.js
@@ -1,3 +1,4 @@
+import { Col, Container, Row } from "react-bootstrap"
 import './App.css';
 import NavigationBar from './components/NavigationBar';
 import "./style/landingPage.css"
@@ -5,9 +6,20 @@ import "./style/landingPage.css"
 function App() {
   return (
     <div>
+      {/* intro section */}
       <div className='MyBG'>
         <NavigationBar />
+        <div className="intro">
+          <Container className="text-white d-flex justify-content-center align-item-center border">
+            <Row>
+              <Col>
+              <h1>NONTON GRATIS</h1>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </div>
+      {/* end of intro */}
     </div>
   );
 }
